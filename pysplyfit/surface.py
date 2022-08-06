af4f16bec8530892cc4eaa3e8d58d64a5fe70ce5
Samuel Scott <ss1870@my.bristol.ac.uk>
2022-08-07 10:12:57 +1200

    First commit

---
+++ pysplyfit/surface.py
@@ -0,0 +1,9 @@
+
+
+class NURBS():
+    """
+    Base class for NURBS objects.
+    Holds info such as degree, knot vectors, control points etc.
+    Methods include setup, basis functions, eval pts.
+    """
+    
